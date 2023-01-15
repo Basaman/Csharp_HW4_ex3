0569a534256f33b978ea8c50faeb08be4a9fb4c2
ilya <ilia.arkhangelsky@gmail.com>
2023-01-15 20:02:08 +0300

    часть кода принимающего числа

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+for (int count = 0; count < 3; count++)
+{
+    int num = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+    
+}
